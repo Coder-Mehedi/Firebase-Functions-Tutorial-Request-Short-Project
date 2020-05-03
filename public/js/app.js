744cc1828ce8a7c66d9974666a018b07ecd3f81e
unknown <mehedi5051@gmail.com>
2020-05-03 20:43:11 +0600

    removed Dummy Function

--- public/js/app.js
+++ public/js/app.js
@@ -10,14 +10,3 @@ requestModal.addEventListener("click", (e) => {
 		requestModal.classList.remove("open");
 	}
 });
-
-// say hello function call
-const button = document.querySelector(".call");
-button.addEventListener("click", () => {
-	// get function reference
-	console.log("clicked");
-	const sayHello = firebase.functions().httpsCallable("sayHello");
-	sayHello({ name: "Mehedi" }).then((result) => {
-		console.log(result.data);
-	});
-});
